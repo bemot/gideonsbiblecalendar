6b486242f24942f80cf5417fa9e21088a6b3f6b4
sasha <sasha@debianmain>
2023-09-21 23:30:56 +0300

    comment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import Bible from "./bible/bible.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Bible />
+    </div>
+  );
+}
+
+export default App;
